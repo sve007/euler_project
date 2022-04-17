3a22a1210cfe0f57035c68a0124bfdbccd88bfd7
Sebastiaan van Eijk <sebastiaanvaneyk@gmail.com>
2022-04-18 00:30:27 +0200

    Add solutions to problems 11, 16, 18, and 67

---
+++ problem11.py
@@ -0,0 +1,60 @@
+# In the 20×20 grid below (see input file), four numbers along a diagonal line have been marked in red.
+
+# The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+#
+# What is the greatest product of four adjacent numbers in the same direction
+# (up, down, left, right, or diagonally) in the 20×20 grid?
+
+with open("input_data/problem11_input.txt") as f:
+    grid = [row.split(" ") for row in f.read().split("\n")]
+
+for i, row in enumerate(grid):
+    grid[i] = [int(j) for j in row]
+
+greatest_prd = 0
+
+chain_size = 4
+# At the edges of the grid, the chain size will be larger than there are numbers left in the grid.
+# However, Python slicing will not throw an error and replace the slicing value with the maximum value
+# of that dimension. Only in the last cases of diagonal chains this will be a problem since we do not use slices
+# and additionally for the left diagonal, negative indices will wrap around.
+for row, _ in enumerate(grid):
+    for col, _ in enumerate(grid):
+
+        # Check horizontal product
+        prd = 1
+        for d in grid[row][col:col+chain_size]:
+            prd = prd*d
+        if prd > greatest_prd:
+            greatest_prd = prd
+
+        # Check vertical product
+        prd = 1
+        for d in [r[col] for r in grid[row:row+chain_size]]:
+            prd = prd * d
+        if prd > greatest_prd:
+            greatest_prd = prd
+
+        # Check right diagonal product.
+        prd = 1
+        for r, c in zip(range(row, row+chain_size), range(col, col+chain_size)):
+            try:
+                prd = prd*grid[r][c]
+            except IndexError:
+                continue
+        if prd > greatest_prd:
+            greatest_prd = prd
+
+        # Check left diagonal product
+        # Here we must be cautious to not include negative indices that will cause the chain to 'wrap around' the grid.
+        # We can fix this by shifting the starting position of the col index by the chain size
+        prd = 1
+        for r, c in zip(range(row, row+chain_size), range(col+chain_size-1, col-1, -1)):
+            try:
+                prd = prd*grid[r][c]
+            except IndexError:
+                continue
+        if prd > greatest_prd:
+            greatest_prd = prd
+
+print(greatest_prd)
---
+++ problem67.py
@@ -0,0 +1,34 @@
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+# the maximum total from top to bottom is 23.
+#
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+#
+# That is, 3 + 7 + 4 + 9 = 23.
+#
+# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'),
+# a 15K text file containing a triangle with one-hundred rows.
+#
+# NOTE: This is a much more difficult version of Problem 18.
+# It is not possible to try every route to solve this problem,
+# as there are 299 altogether! If you could check one trillion (1012) routes every second
+# it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+
+with open("input_data/problem67_input.txt") as f:
+    pyr = f.read().split("\n")
+    for i, row in enumerate(pyr):
+        pyr[i] = [int(j) for j in row.split(" ")]
+
+helper_pyr = pyr[::-1]
+
+for i, row in enumerate(pyr[::-1]):
+    if not (i == 0 or i == (len(pyr)-1)):
+        for j, number in enumerate(row):
+            helper_pyr[i][j] = max(number+helper_pyr[i-1][j], number+helper_pyr[i-1][j+1])
+
+helper_pyr = helper_pyr[::-1]
+max_sum = helper_pyr[0][0] + max(helper_pyr[1])
+
+print(max_sum)
